9cdec0256595a7be2ee02987a062c18f33ffe11a
Ram Vennam <rvennam@users.noreply.github.com>
2017-01-06 09:56:39 -0600

    update PORT env var

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express"),
     app = express();
 
-var port = process.env.VCAP_APP_PORT || 8080;
+var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/public'));
 
